c43d85fe62d016f455e9efc7a9b738e65ae323e3
Preeth Vijay <preethvijay0@gmail.com>
2023-09-26 16:30:59 -0400

    make a WIndow() class to serve as the main window so we can avoid global vars
    the on_close function as been renamed _quit() and moved to the app itself, since it can now be triggered via keypress (q)

--- hammer.py
+++ hammer.py
@@ -1,28 +1,29 @@
 from tkinter import Tk
 
 from hammerpy.gui import HammerPy
-from hammerpy.util import remove_works
 
-def on_close():
-  remove_works(app.works)
-  window.destroy()
+class Window():
+  def __init__(self):
+    self._width = 1080
+    self._height = 720
+    self._bg = "#010012"
+    self._major = 0
+    self._minor = 5
+    self._patch = 1
+
+    # setup window stuff
+    self.root = Tk()
+    self.root.title(f"HammerPy v{self._major}.{self._minor}.{self._patch}")
+    self.root.geometry(f"{self._width}x{self._height}")
+    self.root.minsize = self._width, self._height
+    self.root["bg"] = self._bg
+
+    self.app = HammerPy(self.root, self._width, self._height, self._bg)
+    
+    self.root.protocol("WM_DELETE_WINDOW", self.app._quit)  
+
+    self.root.mainloop()
+
 
 if __name__ == "__main__":
-  # setup window stuff
-  width = 1080
-  height = 720
-  background = "#010012"
-  major = 0
-  minor = 5
-  patch = 0
-
-  window = Tk()
-  window.title(f"HammerPy v{major}.{minor}.{patch}")
-  window.geometry(f"{width}x{height}")
-  window["bg"] = background
-  
-  window.protocol("WM_DELETE_WINDOW", on_close)
-
-  app = HammerPy(window, width, height, background)
-
-  window.mainloop()
+  window = Window()
