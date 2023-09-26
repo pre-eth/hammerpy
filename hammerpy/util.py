650ee0c03f0a2a36ec79e852d75c7fa6590bc482
Preeth Vijay <preethvijay0@gmail.com>
2023-09-26 16:29:07 -0400

    add dataclass declarations in util for greater visibility, and because the list of available institutions is about to grow and include other places. In particular, I'm about to add support for Sotheby's

--- hammerpy/util.py
+++ hammerpy/util.py
@@ -1,82 +1,54 @@
+from dataclasses import dataclass
 from types import FunctionType
-from math import ceil, floor
-from PIL import Image, ImageTk
+from PIL import ImageTk
 from time import sleep
 from urllib.request import urlretrieve
 from re import sub
 from datetime import date
 from os import mkdir, path
 from threading import Thread
-from queue import Queue, Empty
+from queue import Queue
 from os import remove, path
 
 from tkinter import IntVar
 from tkinter.ttk import Label
 
-from hammerpy.gui import Guesswork, HammerPy
+@dataclass
+class Artwork():
+  title: str
+  image_url: str
+  prices: list[int]
 
-def switch_desc(diff_desc: Label, descs: list[str], diff_int: int):
-  diff_desc["text"] = descs[diff_int]
+@dataclass
+class Guesswork():
+  art: Artwork
+  path: str
+  disp_img: ImageTk.PhotoImage
+  review_img: ImageTk.PhotoImage
+  disp_width: int
+  disp_height: int
+  review_width: int
+  review_height: int
+  lower_bound: int
+  upper_bound: int
+  _keep: IntVar
+  _guess: int = 0
 
-def switch_limit(quantity: Label, current_limit: int):
-  new_limit = int(float(current_limit))
-  quantity["text"] = f"{new_limit}".zfill(2)
+  @property
+  def keep(self):
+    return self._keep
 
-def cleanse(sins):
-  return sub("[?:/\"\'\t\n\r!@#$%&<>{}|=+`]", "", sins)
+  @keep.setter
+  def keep(self, value: int):
+    self._keep.set(value)
 
-def remove_works(works: list[Guesswork]):
-  for w in works:
-    if not w.keep.get() and path.isfile(w.path):
-      remove(w.path)
+  @property
+  def guess(self):
+    return self._guess
 
-# separate function in separate thread from HammerPy
-# because main thread must run GUI
-def update_status(h: HammerPy, q: Queue, limit: int):
-  disp_height = h.height - 200
-  factor = (0.05 + (0.1 * h.difficulty.get()))
-  review_width = 500
-
-  while True:
-    try:
-      item = q.get()
-    except Empty:
-      continue
-
-    if not item:
-      break
-    
-    # determine other properties for this work and construct Guesswork object
-
-    work, save_path = item
-
-    # determine dimensions for showing image in guess and result screens
-    # IF THE IMAGE OBJECT ISN'T READ ALL AT ONCE HERE IT DOESN'T LOAD
-    with Image.open(save_path) as img: 
-      width = img.width
-      height = img.height
-      disp_width = float(float(width) / float(height)) * float(disp_height)
-      size1 = img.resize((ceil(disp_width), disp_height))
-      tk_img = ImageTk.PhotoImage(image=size1)
-
-      review_height = 500.0 / (float(width) / float(height))
-      size2 = img.resize((review_width, ceil(review_height)))
-      tk_img2 = ImageTk.PhotoImage(image=size2)
-    
-    lower_bound = floor(work.prices[0] * (1.0 - factor))
-    upper_bound = floor(work.prices[1] * (1.0 + factor))
-    
-    keep = IntVar()
-    keep.set(0)
-
-    guess_work = Guesswork(work, save_path, tk_img, tk_img2, ceil(disp_width), disp_height, 
-                           review_width, ceil(review_height), lower_bound, upper_bound, keep)
-
-    h.works.append(guess_work)
-    h.loading["text"] = f"Fetching artworks... ({len(h.works)}/{limit})\n"
-  
-  # Queue has been read in full, start the actual guessing game
-  h.start_game()
+  @guess.setter
+  def guess(self, value: int):
+    self._guess = value
 
 class Scraper(Thread):
   def __init__(self, queue: Queue, limit: int, scrape_fn: FunctionType):
@@ -111,4 +83,20 @@ class Scraper(Thread):
         sleep(5)
 
     self._q.put_nowait(None)
+
+def switch_desc(diff_desc: Label, descs: list[str], diff_int: int):
+  diff_desc["text"] = descs[diff_int]
+
+def switch_limit(quantity: Label, current_limit: int):
+  new_limit = int(float(current_limit))
+  quantity["text"] = f"{new_limit}".zfill(2)
+
+def cleanse(sins):
+  return sub("[?:/\"\'\t\n\r!@#$%&<>{}|=+`]", "", sins)
+
+def remove_works(works: list[Guesswork]):
+  for w in works:
+    if not w.keep.get() and path.isfile(w.path):
+      remove(w.path)
+
 
