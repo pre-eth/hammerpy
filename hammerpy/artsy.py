2821a9bc22d3afa39f03869b6119ae1742369881
Preeth Vijay <preethvijay0@gmail.com>
2023-09-26 16:32:06 -0400

    add Medium enum to add support for letting the user narrow down the returned works by category :D
    replace single quotes with double quotes if the string is more than just a character

--- hammerpy/artsy.py
+++ hammerpy/artsy.py
@@ -1,17 +1,24 @@
-from dataclasses import dataclass
 from requests import get
 from bs4 import BeautifulSoup
 from math import floor
 from re import sub, findall
 from random import choice, randint
 from urllib.parse import unquote
+from enum import Enum
 
-@dataclass
-class Artwork():
-  title: str
-  image: str
-  prices: list[int]
+from hammerpy.util import Artwork
  
+# Create enum to represent different artwork mediums
+class Medium(Enum):
+  RANDOM = ""
+  PAINTING = "ion/painting"
+  PRINTS = "ion/prints"
+  PHOTOGRAPHY = "ion/photography"
+  SCULPTURE = "ion/sculpture"
+  WORKS_ON_PAPER = "ion/works-on-paper"
+  DESIGN = "ion/design"
+  MIXED_MEDIA = "ion/mixed-media"
+
 def scrape_artsy() -> Artwork:
   agent = "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254"
  
@@ -19,29 +26,29 @@ def scrape_artsy() -> Artwork:
   pagemax = 100         
  
   # for lookup with currency api, tried to include most of the major ones
-  currencies = {'C$':'cad','€':'eur','£':'gbp','HK$':'hkd','¥':'jpy',
-                'ZAR':'zar','CN¥':'cny','BRL':'brl','₱':'php'}
+  currencies = {"C$":"cad","€":"eur","£":"gbp","HK$":"hkd","¥":"jpy",
+                "ZAR":"zar","CN¥":"cny","BRL":"brl","₱":"php"}
 
   l = 0
   while not l:
     # pick a random page and get its content      
     link = f"https://www.artsy.net/collect?page={randint(1, pagemax)}"
     dump = get(link, data={"User-Agent" : agent})
-    dump = BeautifulSoup(dump.text, 'html.parser')
+    dump = BeautifulSoup(dump.text, "html.parser")
 
     # artworkGridItem is identifier for any work on the page, we choose a random one
-    artdivs = dump.find_all('div', attrs={'data-test':'artworkGridItem'})
+    artdivs = dump.find_all("div", attrs={"data-test":"artworkGridItem"})
     if not artdivs:
       continue
     div = choice(artdivs)
-    price = div.findNext('div', attrs={'font-weight':"bold"}).text.replace(',','')
+    price = div.findNext("div", attrs={"font-weight":"bold"}).text.replace(',','')
 
     # get the image
     img_tag = div.findNext('img')
 
     # check if fullsize url is available
-    imgurl = img_tag.get('src')
-    img = imgurl[imgurl.index('https%'):imgurl.rindex('.jpg')+4]
+    imgurl = img_tag.get("src")
+    img = imgurl[imgurl.index("https%"):imgurl.rindex(".jpg")+4]
     img = sub("(larger?)", "normalized", img)
     img = unquote(img)
 
@@ -88,7 +95,7 @@ def scrape_artsy() -> Artwork:
         continue
 
     # format title into "name - 'work' (date)"
-    title = img_tag.get('alt').replace(',', ' -', 1)
+    title = img_tag.get("alt").replace(',', ' -', 1)
     title = title.replace(title[title.rindex(','):title.rindex(',')+2], ' (')+')' 
     
     work = Artwork(title, img, work_prices)
