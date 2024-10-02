54776283c5de17efabc3ab3e9b42d5990cf66359
Preeth Vijay <preethvijay0@gmail.com>
2024-10-02 10:17:32 -0400

    Update file to get 10/10 on pylint and format with black.

--- hammer.py
+++ hammer.py
@@ -1,31 +1,43 @@
+"""Main application runner for HammerPy - entry point"""
+
 from tkinter import Tk
 
 from hammerpy.gui import HammerPy
 
-class Window():
-  def __init__(self):
-    self._width = 1080
-    self._height = 720
-    self._bg = "#010012"
-    self._major = 0
-    self._minor = 5
-    self._patch = 4
 
-    # setup window stuff
-    self.root = Tk()
-    self.root.title(f"HammerPy v{self._major}.{self._minor}.{self._patch}")
-    self.root.geometry(f"{self._width}x{self._height}")
-    self.root.minsize = self._width, self._height
-    self.root["bg"] = self._bg
+class Window:
+    """The game window. Runs thbe main event loop after defining some basic styles"""
 
-    self.app = HammerPy(self.root, self._width, self._height, self._bg)
-    
-    self.root.protocol("WM_DELETE_WINDOW", self.app.quit_game)
-    self.root.bind("<Control_L>+q", self.app.quit_game)
-    self.root.createcommand("::tk::mac::Quit", self.app.quit_game)
+    def __init__(self):
+        self._width = 1080
+        self._height = 720
+        self._bg = "#010012"
+        self.root = Tk()
+        self.app = HammerPy(self.root, self._width, self._height, self._bg)
 
-    self.root.mainloop()
+    def setup_window(self):
+        """Defines style properties and some basic metadata"""
+
+        _major = 0
+        _minor = 5
+        _patch = 4
+
+        self.root.title(f"HammerPy v{_major}.{_minor}.{_patch}")
+        self.root.geometry(f"{self._width}x{self._height}")
+        self.root.minsize = self._width, self._height
+        self.root["bg"] = self._bg
+
+    def run_app(self):
+        """Launches the application"""
+
+        self.root.protocol("WM_DELETE_WINDOW", self.app.quit_game)
+        self.root.bind("<Control_L>+q", self.app.quit_game)
+        self.root.createcommand("::tk::mac::Quit", self.app.quit_game)
+
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
-  window = Window()
+    window = Window()
+    window.setup_window()
+    window.run_app()
