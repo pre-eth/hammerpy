7e62a65adf34c139f4186feb382cf7964a981755
Preeth Vijay <preethvijay0@gmail.com>
2024-10-02 10:30:04 -0400

    update patch version

--- hammer.py
+++ hammer.py
@@ -1,12 +1,11 @@
-"""Main application runner for HammerPy - entry point"""
+"""Main application runner for HammerPy - entry point."""
 
 from tkinter import Tk
-
 from hammerpy.gui import HammerPy
 
 
 class Window:
-    """The game window. Runs thbe main event loop after defining some basic styles"""
+    """The game window. Runs thbe main event loop after defining some basic styles."""
 
     def __init__(self):
         self._width = 1080
@@ -16,11 +15,11 @@ class Window:
         self.app = HammerPy(self.root, self._width, self._height, self._bg)
 
     def setup_window(self):
-        """Defines style properties and some basic metadata"""
+        """Defines style properties and some basic metadata."""
 
         _major = 0
         _minor = 5
-        _patch = 4
+        _patch = 5
 
         self.root.title(f"HammerPy v{_major}.{_minor}.{_patch}")
         self.root.geometry(f"{self._width}x{self._height}")
@@ -28,7 +27,7 @@ class Window:
         self.root["bg"] = self._bg
 
     def run_app(self):
-        """Launches the application"""
+        """Launches the application."""
 
         self.root.protocol("WM_DELETE_WINDOW", self.app.quit_game)
         self.root.bind("<Control_L>+q", self.app.quit_game)
